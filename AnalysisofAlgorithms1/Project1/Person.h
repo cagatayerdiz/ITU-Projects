35671d1c8916f2d8f950460ec6df3d4ee68356cd
Çağatay Erdiz <cagatay_erdiz@hotmail.com>
2015-12-26 08:07:41 +0200

    Create Person.h

---
+++ AnalysisofAlgorithms1/Project1/Person.h
@@ -0,0 +1,23 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+using namespace std;
+
+class Person{
+private:
+	int id;
+	int height;
+	int weight;
+	float BMI;
+	float thresoldDifference;
+public:
+	Person();
+	float calculateBMI();
+	void fillContents(ifstream &, int);
+	float getthresoldDifference(){ return thresoldDifference; }
+	void setthresoldDifference(float value){ thresoldDifference = value; }
+	void writetoFile(ofstream &);
+	~Person();
+};
+
+#endif
